382d521d5c5208d6970eb48661aa2b077b864986
Subhajit Chatterjee <subhajitchatterjee425@gmail.com>
2019-09-12 13:26:04 +0530

    js files for enemy and players are created, player and enemy constructors are create, global objectsare also created

---
+++ js/enemy.js
@@ -0,0 +1,10 @@
+let enemy;//we are creating a global object.
+
+function enemy(enemyType,health,mana,strength,agility,speed){
+    this.enemyType = enemyType;
+    this.health  = health;
+    this.mana = mana;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+}
---
+++ js/gameManager.js
@@ -0,0 +1,18 @@
+let gameManager = {
+/*
+    //now if we do this :-
+    name: function(){
+            let player = new Player():
+    }
+    //then we will be only assigining a value to the player variable and which is availbale everywhere in our program butt....
+
+    //if we do this :-
+
+    name:function(){
+        let player = new Player();
+    }
+
+    //then this will create an object whose scope is only in the gamemanager file and we wont be able to access the file from the other files of the application
+
+*/
+}

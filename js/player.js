7f50cd1f318c57ff23b86e307be1869403cabcb0
Subhajit Chatterjee <subhajitchatterjee425@gmail.com>
2019-09-17 01:08:22 +0530

    playerMoves error fixed by moving the code into the calcAttac constructor

--- js/player.js
+++ js/player.js
@@ -10,44 +10,51 @@ function Player(classType,health,mana,strength,agility,speed){
 }
 
 
-let playerMoves ={
+let playerMoves = {
     calcAttack: function(){
         //who attack first
         let getPlayerSpeed = player.speed;
         let getEnemySpeed = enemy.speed;
-    }
-    //player attacks
-    let playerAttack = function(){
-        let calcBaseDamage;
-        if(player.mana > 0){
-            calcBaseDamage = player.mana * player.strength / 1000;
-        }else{
-            calcBaseDamage = player.agility * player.strength / 1000;
+        //player attacks
+        let playerAttack = function() {
+            let calcBaseDamage;
+            if(player.mana > 0){
+                calcBaseDamage = player.mana * player.strength / 1000;
+            }else{
+                calcBaseDamage = player.agility * player.strength / 1000;
+            }
+            let offsetDammage = Math.floor(Math.random() * Math.floor(10));
+            let calcOutputDamge = calcBaseDamage + offsetDammage;
+            //number of hits
+            let numberOfHits = Math.floor(Math.random() * Math.floor(player.agility / 10) / 2) + 1;//adding 1 so that we hit atleast one time.
+            let attackValues = [calcOutputDamge, numberOfHits];
+            return attackValues;
         }
-        let offsetDammage = Math.floor(Math.random()*Math.floor(10));
-        let calcOutputDamge = calcBaseDamage + offsetDammage;
-        //number of hits
-        let numberOfHits = Math.floor(Math.random()*Math.floor(player.agility/10)/2)+1;//adding 1 so that we hit atleast one time.
-        let attackValues = [calcOutputDamge, numberOfHits];
-        return attackValues;
-    }
-    //enemy attacks !!
-    let enemyAttack = function(){
-        let calcBaseDamage;
-        if(enemy.mana > 0){
-            calcBaseDamage = enemy.mana * enemy.strength / 1000;
-        }else{
-            calcBaseDamage = enemy.agility * enemy.strength / 1000;
+        //enemy attacks !!
+        let enemyAttack = function(){
+            let calcBaseDamage;
+            if(enemy.mana > 0){
+                calcBaseDamage = enemy.mana * enemy.strength / 1000;
+            }else{
+                calcBaseDamage = enemy.agility * enemy.strength / 1000;
+            }
+            let offsetDammage = Math.floor(Math.random() * Math.floor(10));
+            let calcOutputDamge = calcBaseDamage + offsetDammage;
+            //number of hits
+            let numberOfHits = Math.floor(Math.random() * Math.floor(enemy.agility / 10) / 2) + 1;//adding 1 so that we hit atleast one time.
+            let attackValues = [calcOutputDamge, numberOfHits];
+            return attackValues;
         }
-        let offsetDammage = Math.floor(Math.random()*Math.floor(10));
-        let calcOutputDamge = calcBaseDamage + offsetDammage;
-        //number of hits
-        let numberOfHits = Math.floor(Math.random()*Math.floor(enemy.agility/10)/2)+1;//adding 1 so that we hit atleast one time.
-        let attackValues = [calcOutputDamge, numberOfHits];
-        return attackValues;
-    }
 
-    //get player/enemy Health
-    let getPlayerHealth = document.querySelector(".player-health");
-    let getEnemyHealth = document.querySelector(".enemy-health");
+        //get player/enemy Health
+        let getPlayerHealth = document.querySelector(".player-health");
+        let getEnemyHealth = document.querySelector(".enemy-health");
+        //initiate attacks
+        if(getPlayerSpeed >= getEnemySpeed){
+            let playerAttackValues = playerAttack();
+            let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+            enemy.health = enemy.health - totalDamage;
+            alert();
+        }
+    }
 }
